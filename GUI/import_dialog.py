014f9072aa6cfea1a1bac9667cb6a7e499d0d915
PawelPluc <234790@edu.p.lodz.pl>
2023-12-12 21:17:15 +0100

    Added error testing to import_dialog

--- GUI/import_dialog.py
+++ GUI/import_dialog.py
@@ -4,11 +4,19 @@ def import_dialog(data_type = "figure"):
     """
     Displays an input dialog to allow the user to choose the file.
     Should be the same for all types of data, with the only difference being suggesting different names of files by default.
+    Return path to a file
     """
-    print("Choose a file to be loaded")
     Tk().withdraw() 
     file_path = filedialog.askopenfilename(
         title="Select a file",
         filetypes=[("Text files", "*.txt"), ("All files", "*.*")]
     )
     return file_path
+
+
+if __name__ == "__main__":
+    x = import_dialog("figure")
+    print(x)
+
+
+
